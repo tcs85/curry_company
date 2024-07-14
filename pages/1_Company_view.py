956e9d189ac319a3ee28f025758466c6de9ba607
Thiago Souza <144746342+tcs85@users.noreply.github.com>
2024-07-14 11:20:32 -0400

    Update 1_Company_view.py
    changing the filepath to relative

--- pages/1_Company_view.py
+++ pages/1_Company_view.py
@@ -14,7 +14,7 @@ streamlit.set_page_config(page_title="company_view",layout='wide')
 # Import Dataset
 
 
-df=pd.read_csv(r'../FTC_Python/train.csv')
+df=pd.read_csv('train.csv')
 df1=df.copy()
 print(df1.head())
 
